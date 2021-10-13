f39620861f0b550bc669c108e433d23d8af6f864
dialectrical <michael.camele@gmail.com>
2021-10-13 16:52:15 -0700

    first pass at cash-register rewrite Fails test 3, 5, and 6

--- cash-register.js
+++ cash-register.js
@@ -1,132 +1,161 @@
-//CONST sets value of cash tokens
-const TOKEN_VALUE = {"PENNY": 0.01, "NICKEL": 0.05, "DIME": 0.10, "QUARTER": 0.25, "ONE": 1, "FIVE": 5, "TEN": 10, "TWENTY": 20, "ONE HUNDRED": 100};
+const checkCashRegister = (price, cash, cid) => {
+  let drawer = new Map();
+  let changeDue = cash - price;
 
-//determines total value of cash in drawer
-function cidTotal(cid) {
-  let cidValue = 0;
-  for (let i = 0; i < cid.length; i++) {
-    cidValue += cid[i][1];
+  drawer.set("totalValue", 0);
+
+  if (changeDue === 0) {
+    return { status: "OPEN", change: [] };
   }
-    return Math.round(cidValue * 100) / 100;
-}
 
-//determine amount of each token in drawer
-function tokenCount (cid) {
-  //contains amount of each particular token
-  let tokenTotal =
-  [
-    ["PENNY", 0],
+  for (let i = 0; i < cid.length; i++) {
+    let currencyValue;
+    if (cid[i][0] === "PENNY") {
+      currencyValue = 0.01;
+    } else if (cid[i][0] === "NICKEL") {
+      currencyValue = 0.05;
+    } else if (cid[i][0] === "DIME") {
+      currencyValue = 0.1;
+    } else if (cid[i][0] === "QUARTER") {
+      currencyValue = 0.25;
+    } else if (cid[i][0] === "ONE") {
+      currencyValue = 1;
+    } else if (cid[i][0] === "TEN") {
+      currencyValue = 10;
+    } else if (cid[i][0] === "TWENTY") {
+      currencyValue = 20;
+    } else if (cid[i][0] === "ONE HUNDRED") {
+      currencyValue = 100;
+    }
+    drawer.set("totalValue", drawer.get("totalValue") + cid[i][1]);
+    drawer.set(cid[i][0], cid[i][1] / currencyValue);
+  }
+  console.log(drawer.get("totalValue"));
+
+  if (changeDue > drawer.get("totalValue")) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  } else if (changeDue === drawer.get("totalValue")) {
+    return { status: "CLOSED", change: cid.reverse() };
+  }
+
+  let changeArr = [];
+
+  while (changeDue >= 0.01) {
+    if (Math.floor(changeDue / 100) > 0) {
+      if (cid[8][1] >= Math.floor(changeDue / 100) * 100) {
+        changeArr.push(["ONE HUNDRED", Math.floor(changeDue / 100) * 100]);
+        cid[8][1] -= Math.floor(changeDue / 100) * 100;
+        changeDue -= Math.floor(changeDue / 100) * 100;
+      } else {
+        changeArr.push(cid[8]);
+        changeDue -= cid[8][1];
+        cid[8][1] = 0;
+      }
+    }
+    if (Math.floor(changeDue / 20) > 0) {
+      if (cid[7][1] >= Math.floor(changeDue / 20) * 20) {
+        changeArr.push(["TWENTY", Math.floor(changeDue / 20) * 20]);
+        cid[7][1] -= Math.floor(changeDue / 20) * 20;
+        changeDue -= Math.floor(changeDue / 20) * 20;
+      } else {
+        changeArr.push(cid[7].slice().slice());
+        changeDue -= cid[7][1];
+        cid[7][1] = 0;
+      }
+    }
+    if (Math.floor(changeDue / 10) > 0) {
+      if (cid[6][1] >= Math.floor(changeDue / 10) * 10) {
+        changeArr.push(["TEN", Math.floor(changeDue / 10) * 10]);
+        cid[6][1] -= Math.floor(changeDue / 10) * 10;
+        changeDue -= Math.floor(changeDue / 10) * 10;
+      } else {
+        changeArr.push(cid[6].slice());
+        changeDue -= cid[6][1];
+        cid[6][1] = 0;
+      }
+    }
+    if (Math.floor(changeDue / 5) > 0) {
+      if (cid[5][1] >= Math.floor(changeDue / 5) * 5) {
+        changeArr.push(["FIVE", Math.floor(changeDue / 5) * 5]);
+        cid[5][1] -= Math.floor(changeDue / 5) * 5;
+        changeDue -= Math.floor(changeDue / 5) * 5;
+      } else {
+        changeArr.push(cid[5].slice());
+        changeDue -= cid[5][1];
+        cid[5][1] = 0;
+      }
+    }
+    if (Math.floor(changeDue / 1) > 0) {
+      if (cid[4][1] >= Math.floor(changeDue / 1) * 1) {
+        changeArr.push(["ONE", Math.floor(changeDue / 1)]);
+        cid[4][1] -= Math.floor(changeDue / 1) * 1;
+        changeDue -= Math.floor(changeDue / 1) * 1;
+      } else {
+        changeArr.push(cid[4].slice());
+        changeDue -= cid[4][1];
+        cid[4][1] = 0;
+      }
+    }
+    if (Math.floor(changeDue / 0.25) > 0) {
+      if (cid[3][1] >= Math.floor(changeDue / 0.25) * 0.25) {
+        changeArr.push(["QUARTER", Math.floor(changeDue / 0.25) * 0.25]);
+        cid[3][1] -= Math.floor(changeDue / 0.25) * 0.25;
+        changeDue -= Math.floor(changeDue / 0.25) * 0.25;
+      } else {
+        changeArr.push(cid[3].slice());
+        changeDue -= cid[3][1];
+        cid[3][1] = 0;
+      }
+    }
+    if (Math.floor(changeDue / 0.1) > 0) {
+      if (cid[2][1] >= Math.floor(changeDue / 0.1) * 0.1) {
+        changeArr.push(["DIME", Math.floor(changeDue / 0.1) * 0.1]);
+        cid[2][1] -= Math.floor(changeDue / 0.1) * 0.1;
+        changeDue -= Math.floor(changeDue / 0.1) * 0.1;
+      } else {
+        changeArr.push(cid[2].slice());
+        changeDue -= cid[2][1];
+        cid[2][1] = 0;
+      }
+    }
+    if (Math.floor(changeDue / 0.05) > 0) {
+      if (cid[1][1] >= Math.floor(changeDue / 0.05) * 0.05) {
+        changeArr.push(["NICKEL", Math.floor(changeDue / 0.05) * 0.05]);
+        cid[1][1] -= Math.floor(changeDue / 0.05) * 0.05;
+        changeDue -= Math.floor(changeDue / 0.05) * 0.05;
+      } else {
+        changeArr.push(cid[1].slice());
+        changeDue -= cid[1][1];
+        cid[1][1] = 0;
+      }
+    }
+    if (Math.floor(changeDue / 0.01) > 0) {
+      if (cid[0][1] >= Math.floor(changeDue / 0.01) * 0.01) {
+        changeArr.push(["PENNY", Math.floor(changeDue / 0.01) * 0.01]);
+        cid[0][1] -= Math.floor(changeDue / 0.01) * 0.01;
+        changeDue -= Math.floor(changeDue / 0.01) * 0.01;
+      } else {
+        changeArr.push(cid[0].slice());
+        changeDue -= cid[0][1];
+        cid[0][1] = 0;
+      }
+    }
+  }
+
+  console.log({ status: "OPEN", change: changeArr });
+  return { status: "OPEN", change: changeArr };
+};
+
+console.log(
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 0.01],
     ["NICKEL", 0],
     ["DIME", 0],
     ["QUARTER", 0],
-    ["ONE", 0],
+    ["ONE", 1],
     ["FIVE", 0],
     ["TEN", 0],
     ["TWENTY", 0],
-    ["ONE HUNDRED", 0]
-  ];
-  //loop through cid array and calculate total amount of each token
-  for (let i = 0; i < cid.length; i++) {
-    tokenTotal[i][1] += Math.round((cid[i][1] / TOKEN_VALUE[cid[i][0]]) * 100) / 100;
-  }
-  return tokenTotal;
-}
-
-//detremines if it possible to return exact change for a transaction
-function makeChange (price, cash, cid) {
-  var tokenAmount = tokenCount (cid);
-  let changeDue = cash - price;
-  let changeGiven =
-    [
-      ["PENNY", 0],
-      ["NICKEL", 0],
-      ["DIME", 0],
-      ["QUARTER", 0],
-      ["ONE", 0],
-      ["FIVE", 0],
-      ["TEN", 0],
-      ["TWENTY", 0],
-      ["ONE HUNDRED", 0]
-    ];
-    for(var i = cid.length; i > 0; i--) {
-      var tokenNeeded = Math.floor(changeDue / TOKEN_VALUE[cid[i - 1][0]]);
-      if (tokenAmount[i - 1][1] - tokenNeeded < 0) {
-        changeDue -= (TOKEN_VALUE[cid[i - 1][0]] * tokenAmount[i - 1][1]);
-        changeGiven[i-1][1] += (TOKEN_VALUE[cid[i - 1][0]] * tokenAmount[i - 1][1]);
-        tokenAmount[i - 1][1] = 0;
-      } else {
-        tokenAmount[i - 1][1] -= tokenNeeded;
-        changeDue -= (TOKEN_VALUE[cid[i - 1][0]] * tokenNeeded);
-        changeGiven[i - 1][1] += (TOKEN_VALUE[cid[i - 1][0]] * tokenNeeded);
-      }
-    }
-    if (Math.round(changeDue * 100) / 100 === 0) {
-      return true;
-    } else {
-      return {status: "INSUFFICIENT_FUNDS", change: []};
-    }
-}
-
-//split sorter
-function changeSort(changeGiven) {
-  let changeSort = [];
-  let changeArray = [];
-  //first, remove any 0s and pass non-0s to changesort
-  for (let i = 0; i < changeGiven.length; i++) {
-    if (changeGiven[i][1] > 0) {
-      changeSort.push(changeGiven[i]);
-    }
-  }
-  //then sort it in descending order!
-  changeSort.sort(function (a,b){return b-a});
-  for (let i = 0; i < changeSort.length; i++) {
-    changeArray.push(changeSort[i]);
-  }
-  return changeArray;
-}
-
-function checkCashRegister(price, cash, cid) {
-  if (cidTotal(cid) < price - cash) {
-
-    return {status: "INSUFFICIENT_FUNDS", change: []};
-  } else if (cidTotal(cid) == cash - price){
-    return {status: "CLOSED", change: cid};
-  } else{
-    let tokenAmount = tokenCount (cid);
-    let changeDue = cash - price;
-    let changeGiven =
-      [
-        ["PENNY", 0],
-        ["NICKEL", 0],
-        ["DIME", 0],
-        ["QUARTER", 0],
-        ["ONE", 0],
-        ["FIVE", 0],
-        ["TEN", 0],
-        ["TWENTY", 0],
-        ["ONE HUNDRED", 0]
-      ];
-    for(let i = cid.length; i > 0; i--) {
-      let tokenNeeded = Math.floor(changeDue / TOKEN_VALUE[cid[i - 1][0]]);
-      if (tokenAmount[i - 1][1] - tokenNeeded < 0) {
-        changeDue -= (TOKEN_VALUE[cid[i - 1][0]] * tokenAmount[i - 1][1]);
-        changeGiven[i-1][1] += (TOKEN_VALUE[cid[i - 1][0]] * tokenAmount[i - 1][1]);
-        tokenAmount[i - 1][1] = 0;
-      } else {
-        tokenAmount[i - 1][1] -= tokenNeeded;
-        changeDue -= (TOKEN_VALUE[cid[i - 1][0]] * tokenNeeded);
-        changeGiven[i - 1][1] += (TOKEN_VALUE[cid[i - 1][0]] * tokenNeeded);
-      }
-    }
-    console.log(changeDue);
-    if (changeDue < 0.01 && changeDue != 0) { //brute force solution to issue with decimal values
-        changeGiven[0][1] += 0.01;
-      }
-    if (Math.round(changeDue * 100) / 100 === 0 || changeDue < 0.1) {
-      return {status: "OPEN", change: changeSort(changeGiven)};
-    } else {
-      console.log(changeDue);
-      return {status: "INSUFFICIENT_FUNDS", change: []};
-    }
-  }
-}
+    ["ONE HUNDRED", 0],
+  ])
+);
